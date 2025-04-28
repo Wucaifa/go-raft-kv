ad4c1c4fb356d92f0076c561e0fd1c8110b11de4
root <root@LAPTOP-FUTGN67B>
2025-04-28 23:56:48 +0800

    全局变量文件

---
+++ server/global/global.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	kv_raft "go_raft_kv"
+	"go_raft_kv/raft"
+	"go_raft_kv/server/storage"
+)
+
+var (
+	Config         *kv_raft.Config                // 配置文件
+	R              *raft.Raft                     // Raft 实例
+	ClientCommands chan string                    // 客户端命令通道
+	StorageEngine  storage.StorageEngineInterface // 存储引擎
+	Reqs           = make(map[string]chan string) // 请求通道
+)
+
+const (
+	Success = "success" // 成功
+	Failed  = "failed"  // 失败
+	Forward = "forward" // 转发
+)
